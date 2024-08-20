2219cbda0762b36ffdc8e02d0d3276b4e9a2a7f4
Thorben B. <thorbenbunger@gmail.com>
2024-08-20 17:27:47 +0200

    feat: add pod deployment test to main file

--- src/main/java/dev/thorben/Main.java
+++ src/main/java/dev/thorben/Main.java
@@ -1,11 +1,10 @@
 package dev.thorben;
 
+import dev.thorben.cli.conversations.VanillaPodConfigurationConversation;
 import io.kubernetes.client.openapi.ApiClient;
 import io.kubernetes.client.openapi.ApiException;
 import io.kubernetes.client.openapi.Configuration;
 import io.kubernetes.client.openapi.apis.AppsV1Api;
-import io.kubernetes.client.openapi.apis.CoreV1Api;
-import io.kubernetes.client.openapi.models.*;
 import io.kubernetes.client.util.ClientBuilder;
 import io.kubernetes.client.util.KubeConfig;
 
@@ -18,35 +17,11 @@ public class Main {
 
         ApiClient client = ClientBuilder.kubeconfig(KubeConfig.loadKubeConfig(new FileReader("/etc/rancher/k3s/k3s.yaml"))).build();
         Configuration.setDefaultApiClient(client);
-        CoreV1Api api = new CoreV1Api();
-        V1PodList list =
-                api.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null, null);
-        for (V1Pod item : list.getItems()) {
-            System.out.println(item.getMetadata().getName());
-        }
 
-        V1Deployment deployment = new V1Deployment()
-                .apiVersion("apps/v1")
-                .kind("Deployment")
-                .metadata(new V1ObjectMeta().name("minecraft-deployment"))
-                .spec(new V1DeploymentSpec()
-                        .replicas(3)
-                        .selector(new V1LabelSelector()
-                                .putMatchLabelsItem("app", "minecraft"))
-                        .template(new V1PodTemplateSpec()
-                                .metadata(new V1ObjectMeta()
-                                        .putLabelsItem("app", "minecraft"))
-                                .spec(new V1PodSpec()
-                                        .containers(java.util.Collections.singletonList(new V1Container()
-                                                .name("minecraft-container")
-                                                .image("fourtyseven/minecraft_server_arm:latest")
-                                                .ports(java.util.Collections.singletonList(new V1ContainerPort()
-                                                        .containerPort(25565))))))));
+        VanillaPodConfigurationConversation vanillaPodConfigurationConversation = new VanillaPodConfigurationConversation();
+        vanillaPodConfigurationConversation.start();
 
         AppsV1Api apiInstance = new AppsV1Api();
-        V1Deployment result = apiInstance.createNamespacedDeployment("default", deployment, null, null, null, null);
-        System.out.println("Deployment created: " + result.getMetadata().getName());
-
-        System.out.print("Hello Afterworld");
+        apiInstance.createNamespacedDeployment("default", vanillaPodConfigurationConversation.getDeployment(), null, null, null, null);
     }
 }
