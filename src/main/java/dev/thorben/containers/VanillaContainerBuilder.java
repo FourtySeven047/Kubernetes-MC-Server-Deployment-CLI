868535fd0db5d4d2f257e6d931e1f6da9d41ff5b
Thorben B. <thorbenbunger@gmail.com>
2024-08-20 17:27:23 +0200

    feat: adapt Logic of Builder's

--- src/main/java/dev/thorben/containers/VanillaContainerBuilder.java
+++ src/main/java/dev/thorben/containers/VanillaContainerBuilder.java
@@ -1,22 +1,18 @@
 package dev.thorben.containers;
 
-import io.kubernetes.client.custom.Quantity;
 import io.kubernetes.client.openapi.models.V1Container;
 import io.kubernetes.client.openapi.models.V1ContainerPort;
 import io.kubernetes.client.openapi.models.V1EnvVar;
-import io.kubernetes.client.openapi.models.V1ResourceRequirements;
 
 public class VanillaContainerBuilder {
 
-    private String version;
-    private String cpu;
-    private String memory;
-    private Integer port;
+    private String name = "minecraft-server";
+    private String version = "1.12.2";
+    private Integer port = 25564;
 
-    public VanillaContainerBuilder(String version, String cpu, String memory, Integer port) {
+    public VanillaContainerBuilder(String name, String version, Integer port) {
+        this.name = name;
         this.version = version;
-        this.cpu = cpu;
-        this.memory = memory;
         this.port = port;
     }
 
@@ -25,60 +21,38 @@ public class VanillaContainerBuilder {
     }
 
     public V1Container build() {
-        V1Container container = new V1Container();
-        container.setImage("fourtyseven/minecraft_server");
-        container.setName("minecraft-server:" + version);
+        return new V1Container()
+                .name(name)
+                .image("fourtyseven/minecraft_server_arm:latest")
+                .addEnvItem(new V1EnvVar()
+                        .name("version")
+                        .value(version))
+                .ports(java.util.Collections.singletonList(new V1ContainerPort()
+                        .containerPort(25565)
+                        .hostPort(port)));
+    }
 
-        V1ResourceRequirements resources = new V1ResourceRequirements();
-        resources.putLimitsItem("cpu", new Quantity(cpu)); // 1
-        resources.putLimitsItem("memory", new Quantity(memory + "Mi")); // 1Gi
-        container.setResources(resources);
-
-        container.addEnvItem(new V1EnvVar().name("version").value(version));
-
-        V1ContainerPort containerPort = new V1ContainerPort();
-        containerPort.setContainerPort(25565);
-        containerPort.setHostPort(port);
-        container.addPortsItem(containerPort);
-
-        container.addPortsItem(containerPort);
-
-        return container;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public void setVersion(String version) {
         this.version = version;
     }
 
-    public void setCpu(String cpu) {
-        this.cpu = cpu;
-    }
-
-    public void setMemory(String memory) {
-        this.memory = memory;
-    }
-
     public void setPort(Integer port) {
         this.port = port;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public String getVersion() {
         return version;
     }
 
-    public String getCpu() {
-        return cpu;
-    }
-
-    public String getMemory() {
-        return memory;
-    }
-
     public Integer getPort() {
         return port;
-    }
-
-    public boolean isComplete() {
-        return version != null && cpu != null && memory != null && port != null;
     }
 }
--- src/main/java/dev/thorben/pods/VanillaPodBuilder.java
+++ src/main/java/dev/thorben/pods/VanillaPodBuilder.java
@@ -2,32 +2,45 @@ package dev.thorben.pods;
 
 import io.kubernetes.client.openapi.models.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class VanillaPodBuilder {
 
-    private final V1Pod pod = new V1Pod();
-    V1PodSpec spec = new V1PodSpec();
-    V1ObjectMeta meta = new V1ObjectMeta();
+    private String podName;
+    List<V1Container> containers = new ArrayList<>();
 
     public VanillaPodBuilder() {
 
     }
 
     public void addContainer(V1Container container) {
-        spec.addContainersItem(container);
+        containers.add(container);
     }
 
-    public void setPodName(String name) {
-        meta.setName(name);
+    public void setPodName(String podName) {
+        this.podName = podName;
     }
 
-    public void addLabel(String key, String value) {
-        meta.putLabelsItem(key, value);
-    }
+    public V1Deployment build() {
+        if (containers.isEmpty()) {
+            System.out.print("Building process stopped. No containers have been added to the pod.");
+            return null;
+        }
+        V1Deployment deployment = new V1Deployment()
+                .apiVersion("apps/v1")
+                .kind("Deployment")
+                .metadata(new V1ObjectMeta().name(podName))
+                .spec(new V1DeploymentSpec()
+                        .replicas(1)
+                        .selector(new V1LabelSelector()
+                                .putMatchLabelsItem("app", "minecraft"))
+                        .template(new V1PodTemplateSpec()
+                                .metadata(new V1ObjectMeta()
+                                        .putLabelsItem("app", "minecraft"))
+                                .spec(new V1PodSpec().containers(containers))));
 
-    public V1Pod build() {
-        pod.setSpec(spec);
-        pod.setMetadata(meta);
-
-        return pod;
+        //AppsV1Api apiInstance = new AppsV1Api();
+        return deployment; //apiInstance.createNamespacedDeployment("default", deployment, null, null, null, null);
     }
 }
