16da44452abde2bce838ba0bf18cebdb2902e862
Thorben B. <thorbenbunger@gmail.com>
2024-08-23 23:54:12 +0200

    feat: add listing of active deployments

--- src/main/java/dev/thorben/deployment/DeploymentManager.java
+++ src/main/java/dev/thorben/deployment/DeploymentManager.java
@@ -1,16 +1,27 @@
 package dev.thorben.deployment;
 
-import io.kubernetes.client.openapi.ApiException;
+import dev.thorben.configuration.Config;
 import io.kubernetes.client.openapi.apis.AppsV1Api;
 import io.kubernetes.client.openapi.models.V1Deployment;
+
+import java.util.List;
 
 public class DeploymentManager {
 
     public static void deploy(V1Deployment deployment) {
         AppsV1Api apiInstance = new AppsV1Api();
         try {
-            apiInstance.createNamespacedDeployment("default", deployment, null, null, null, null);
-        } catch (ApiException e) {
+            apiInstance.createNamespacedDeployment(Config.readFromFile("namespace"), deployment, null, null, null, null);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static List<V1Deployment> listActiveDeployments() {
+        AppsV1Api apiInstance = new AppsV1Api();
+        try {
+            return apiInstance.listNamespacedDeployment(Config.readFromFile("namespace"), null, null, null, null, null, null, null, null, null, null).getItems();
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
