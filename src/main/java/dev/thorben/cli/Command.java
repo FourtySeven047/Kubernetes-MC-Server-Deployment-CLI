d6ed4ba678efe9bbb1c8effa140bbbda76d52bcb
Thorben B. <thorbenbunger@gmail.com>
2024-08-24 14:39:17 +0200

    feat add Command interface and CommandFactory

---
+++ src/main/java/dev/thorben/cli/Command.java
@@ -0,0 +1,6 @@
+package dev.thorben.cli;
+
+public interface Command {
+
+    void execute(String[] args);
+}
---
+++ src/main/java/dev/thorben/cli/CommandFactory.java
@@ -0,0 +1,30 @@
+package dev.thorben.cli;
+
+import dev.thorben.cli.commands.*;
+import dev.thorben.system.Config;
+
+public class CommandFactory {
+
+    public static Command getCommand(String[] args) {
+        if (args.length == 0) {
+            return new EmptyCommand();
+        }
+
+        if (args[0].equals("deploy") && args.length > 1) {
+            System.out.println("No deployment type provided. Exiting...");
+            if (args[1].equals("vanilla")) {
+                return new VanillaDeploymentCommand();
+            }
+        } else if (args[0].equals("config") && args.length > 1) {
+            if (args[1].equals("change")) {
+                Config.reconfigure();
+                return new ChangeConfigCommand();
+            }
+        } else if (args[0].equals("ps")) {
+            return new ProcessStatusCommand();
+        } else if (args[0].equals("help")) {
+            return new HelpCommand();
+        }
+        return new EmptyCommand();
+    }
+}
