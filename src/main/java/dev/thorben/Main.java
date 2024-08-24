b9b1c44af1c27b2f6cc7928f66a3e812d4d551f5
Thorben B. <thorbenbunger@gmail.com>
2024-08-24 14:41:20 +0200

    feat: implement changes

--- src/main/java/dev/thorben/Main.java
+++ src/main/java/dev/thorben/Main.java
@@ -1,9 +1,9 @@
 package dev.thorben;
 
-import dev.thorben.cli.conversations.VanillaPodConfigurationConversation;
+import dev.thorben.cli.CommandFactory;
 import dev.thorben.system.Config;
-import dev.thorben.deployment.DeploymentManager;
 import dev.thorben.system.ErrorHandling;
+
 import io.kubernetes.client.openapi.ApiClient;
 import io.kubernetes.client.openapi.ApiException;
 import io.kubernetes.client.openapi.Configuration;
@@ -18,50 +18,39 @@ import java.util.List;
 import java.util.Optional;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        Config.init();
+    public static void main(String[] args) {
         //ApiClient client = ClientBuilder.kubeconfig(KubeConfig.loadKubeConfig(new FileReader("/etc/rancher/k3s/k3s.yaml"))).build();
-        ApiClient client = ClientBuilder.kubeconfig(KubeConfig.loadKubeConfig(new FileReader(Config.readFromFile("kubeconfig_path")))).build();
-        Configuration.setDefaultApiClient(client);
-        CoreV1Api api = new CoreV1Api();
-        List<V1Namespace> namespaces = api.listNamespace(null, null, null, null, null, null, null, null, null, null).getItems();
-        namespaces.stream().filter(namespace -> namespace.getMetadata().getName().equals(Config.readFromFile("namespace"))).findFirst().or(() -> {
-            try {
-                V1Namespace v1Namespace = new V1Namespace();
-                V1ObjectMeta metadata = new V1ObjectMeta();
-                metadata.setName(Config.readFromFile("namespace"));
-                v1Namespace.setMetadata(metadata);
-                v1Namespace.setApiVersion("v1");
-                v1Namespace.setKind("Namespace");
-                api.createNamespace(v1Namespace, null, null, null, null);
-            } catch (ApiException e) {
-                ErrorHandling.handle("An error occurred while creating the namespace:", e);
-                e.printStackTrace();
-            }
-            return Optional.empty();
-        });
-        if (args.length == 0) {
-            System.out.println("No arguments provided. Exiting...");
+        Config.init();
+        try {
+            ApiClient client = ClientBuilder.kubeconfig(KubeConfig.loadKubeConfig(new FileReader(Config.readFromFile("kubeconfig_path")))).build();
+            Configuration.setDefaultApiClient(client);
+        } catch (Exception e) {
+            ErrorHandling.handle("An error occurred while loading the kubeconfig file:", e);
             return;
         }
-        if (args[0].equals("deploy") && args.length > 1) {
-            if (args[1].equals("vanilla")) {
-                VanillaPodConfigurationConversation vanillaPodConfigurationConversation = new VanillaPodConfigurationConversation();
-                vanillaPodConfigurationConversation.start();
-                DeploymentManager.deploy(vanillaPodConfigurationConversation.getDeployment());
-                return;
-            }
-            System.out.println("Deployment type not recognized. Exiting...");
-        } else if (args[0].equals("config") && args.length > 1) {
-            if (args[1].equals("change")) {
-                Config.reconfigure();
-                return;
-            }
-            return;
-        } else if (args[0].equals("ps")) {
-            DeploymentManager.printActiveDeployments();
+
+        try {
+            CoreV1Api api = new CoreV1Api();
+            List<V1Namespace> namespaces = api.listNamespace(null, null, null, null, null, null, null, null, null, null).getItems();
+            namespaces.stream().filter(namespace -> namespace.getMetadata().getName().equals(Config.readFromFile("namespace"))).findFirst().or(() -> {
+                try {
+                    V1Namespace v1Namespace = new V1Namespace();
+                    V1ObjectMeta metadata = new V1ObjectMeta();
+                    metadata.setName(Config.readFromFile("namespace"));
+                    v1Namespace.setMetadata(metadata);
+                    v1Namespace.setApiVersion("v1");
+                    v1Namespace.setKind("Namespace");
+                    api.createNamespace(v1Namespace, null, null, null, null);
+                } catch (ApiException e) {
+                    ErrorHandling.handle("An error occurred while creating the namespace:", e);
+                }
+                return Optional.empty();
+            });
+        } catch (ApiException e) {
+            ErrorHandling.handle("An error occurred while the namespace creation:", e);
             return;
         }
-        System.out.println("Command not recognized. Exiting...");
+
+        CommandFactory.getCommand(args).execute(args);
     }
 }
