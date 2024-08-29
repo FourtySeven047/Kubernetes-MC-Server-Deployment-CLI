8275aa7702b164b2c806caeb8fd64f432b13c24e
Thorben B. <thorbenbunger@gmail.com>
2024-08-29 21:19:19 +0200

    feat(DeleteDeploymentCommand): add safety feature

--- src/main/java/dev/thorben/cli/commands/DeleteDeploymentCommand.java
+++ src/main/java/dev/thorben/cli/commands/DeleteDeploymentCommand.java
@@ -1,15 +1,26 @@
 package dev.thorben.cli.commands;
 
+import dev.thorben.Main;
 import dev.thorben.cli.Command;
 import dev.thorben.deployment.DeploymentManager;
 import io.kubernetes.client.openapi.models.V1Deployment;
+
+import java.util.Scanner;
 
 public class DeleteDeploymentCommand implements Command {
 
     @Override
     public void execute(String[] args) {
+        Main.initKubernetes();
         V1Deployment deployment = DeploymentManager.selectActiveDeployment();
         if (deployment == null) {
+            return;
+        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Are you sure you want to delete the deployment " + deployment.getMetadata().getName() + "? (y/n) ");
+        String confirmation = scanner.nextLine();
+        if (!confirmation.equalsIgnoreCase("y")) {
+            System.out.println("Deletion aborted.");
             return;
         }
         DeploymentManager.deleteDeployment(deployment.getMetadata().getNamespace(), deployment.getMetadata().getName());
